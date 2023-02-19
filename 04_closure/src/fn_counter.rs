7e8e6b375ad5b97f37946d7ff18b3f589821ef78
AarenWang <AarenWang@sina.com>
2023-02-19 17:03:56 +0800

    04_closure

--- 04_closure/src/fn_counter.rs
+++ 04_closure/src/fn_counter.rs
@@ -19,6 +19,8 @@ fn main() {
     //let f = counter(2);
     let f = counter(2);
     assert_eq!(3, f(1));
+    assert_eq!(4, f(2));
+    
 
     let add = |a: i32, b: i32| -> i32 { a + b };
     assert_eq!(3, add(1, 2));
@@ -30,7 +32,9 @@ fn main() {
     //闭包参数可以为一个闭包
     let add2 = |a: fn() -> i32, b: i32, c: i32| (a)() + b + c;
     let r = add2(five, 2, 3);
+    let r = add2(|| 5, 2, 3);
     assert_eq!(10, r);
 
     // let c1: () = || { println! ("i am a closure") };
+    
 }
